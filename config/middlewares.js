ca3641ee1ec19e772e3c1037827cd740e56e6c84
Kundan Kumar <c-kundan.kumar@timesgroup.com>
2023-01-21 13:05:57 +0530

    cors issue fixed

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,21 @@
 module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+    'strapi::errors',
+    'strapi::security',
+    'strapi::poweredBy',
+    {
+        name: 'strapi::cors',
+        config: {
+            enabled: true,
+            header: '*',
+            origin: ['*'],
+            keepHeaderOnError: true,
+            methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS']
+        }
+    },
+    'strapi::logger',
+    'strapi::query',
+    'strapi::body',
+    'strapi::session',
+    'strapi::favicon',
+    'strapi::public',
 ];
